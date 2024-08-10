ce4446a786ac93395f8b08cae827fc771e7cb2fd
YunJaeHoon <sleepzzz214@naver.com>
2024-08-11 01:09:20 +0900

    FIX: CORS 설정 수정

--- src/main/java/com/example/speed_you_back/configuration/WebConfig.java
+++ src/main/java/com/example/speed_you_back/configuration/WebConfig.java
@@ -14,11 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
         
         // CORS 설정
         registry
-                .addMapping("/**")                                  // 해당 설정을 적용할 URL
-                .allowedOrigins("https://speed-you.netlify.app")    // 자원 공유를 허락할 기기의 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE")     // 허용할 HTTP method
-                .allowCredentials(true)                             // 쿠키 요청 허용
-                .allowedHeaders("*");                               // CORS 요청에 허용된 헤더
+                .addMapping("/**")                                     // 해당 설정을 적용할 URL
+                .allowedOrigins("https://speed-you.netlify.app/**")    // 자원 공유를 허락할 기기의 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE")        // 허용할 HTTP method
+                .allowCredentials(true)                                // 쿠키 요청 허용
+                .allowedHeaders("*");                                  // CORS 요청에 허용된 헤더
 
     }
 
